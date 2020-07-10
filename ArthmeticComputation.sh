3dced564c16b153952381221a731d8d63193a490
Rohini657 <rohinijadhav3196@gmail.com>
2020-07-10 14:39:43 +0530

    First Commit

---
+++ ArthmeticComputation.sh
@@ -0,0 +1,13 @@
+#####Arthmetic Operation#####
+#### Use Case1
+#!/bin/bash -x
+read -p "Enter First Numbeer : " a;
+read -p "Enter Second Number : " b;
+read -p "Enter Third Number :  " c;
+
+
+
+
+
+
+
